bb2fc678d4d742ca9e0b02c48202bec28f81b0e8
chistym17 <mchisty31@gmail.com>
2023-11-18 21:51:40 +0600

    react helmet added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Helmet } from "react-helmet-async"
 import Categories from "../../components/Categories/Categories"
 import Rooms from "../../components/Room/Rooms"
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>StayVista | Vacation Homes & Condo Rentals</title>
+      </Helmet>
       <h1>Welcome to StayVista</h1>
       <Categories></Categories>
       <Rooms></Rooms>
